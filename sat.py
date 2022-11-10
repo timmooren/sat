a8557923771dcd3f7c1b0ee14e62bdc0f7255069
Tim Mooren <timmooren9@gmail.com>
2022-11-10 13:14:55 +0100

    finished dpll

--- sat.py
+++ sat.py
@@ -32,30 +32,33 @@ class SAT():
         literals_positive = set()
         literals_negative = set()
 
+
         if literal:
             for clause in self.cnf.copy():
                 # remove clauses containing literal
                 if literal in clause:
                     self.cnf.remove(clause)
 
-
                 # shorten clauses containing ~literal
                 if -literal in clause:
                     clause.remove(-literal)
 
+        print(f'before {self.cnf}')
         # if it contains no clauses
         if self.cnf == []:
+            print('im here  ')
             return True
 
-        for clause in self.cnf.copy():
-            # if it contains an empty clause
-            if clause == []:
-                return False
+        # if it contains an empty clause
+        if any(clause == [] for clause in self.cnf):
+            return False
 
+
+        for clause in self.cnf.copy():
             # if it contains a unit clause, add it to assignments and restart
             if len(clause) == 1:
                 self.assignments.extend(clause)
-                self.dpll(literal=clause[0])
+                return self.dpll(literal=clause[0])
 
             # # if it contains tautology (p v ~p), remove it and restart
             # if len(clause) == 2:
@@ -64,22 +67,26 @@ class SAT():
             #         self.cnf.remove(clause)
             #         self.dpll()
 
+
             # add all literals to set
             for literal in clause:
                 literals_positive.add(
                     literal) if literal > 0 else literals_negative.add(abs(literal))
 
         # if it contains a pure literal, add it to assignments, remove it and restart
-        set_difference = literals_negative - literals_positive
-        if set_difference:
-            pure_literal = set_difference.pop()
-            self.dpll(literal=pure_literal)
+        pure_literals = literals_negative ^ literals_positive
+        if pure_literals:
+            pure_literal = pure_literals.pop()
+            return self.dpll(literal=pure_literal)
 
         # pick a literal and restart
-        print(self.cnf)
+        print(f' after = {self.cnf}')
+        print()
         literal = self.cnf[0][0]
 
         return True if self.dpll(literal) else self.dpll(-literal)
+
+
 
 
     # TODO
@@ -91,4 +98,4 @@ if __name__ == '__main__':
     solver = SAT(cnf=[[1, 2], [-1, 2], [-2, 3], [-3, 1]])
     satisfaction = solver.dpll()
     print(satisfaction)
-    # print(solver.assignments)
+    print(solver.assignments)
