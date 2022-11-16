f2d443c03b3cc38e02fd05b9bb2d7ea249525e4b
rnambaa <ryo.namba@gmail.com>
2022-11-16 18:05:49 +0100

    small modif to parse.py to correct DIMACS translation

--- parse.py
+++ parse.py
@@ -24,9 +24,10 @@ def read_dimacs(filename: str):
             # ignore the last number
             clause = [int(literal) for literal in line.split()[:-1]]
             clauses.append(clause)
-
+    # removes last clause (game_to_DIMACS.py accidentally makes extra empty clause)
+    del clauses[-1]
     return clauses, n_vars, n_clauses
 
 
-clauses, n_vars, n_clauses = read_dimacs(filename='examples/sudoku1.cnf')
-print(len(clauses), n_clauses)
+clauses, n_vars, n_clauses = read_dimacs(filename='DIMACS_4x4/sudoku_4x4_nr_1.cnf')
+print(clauses)
