4cd4a8d351364e227a2c2715adb7db06b901e706
Tim Mooren <timmooren9@gmail.com>
2022-11-18 14:25:06 +0100

    updated parse

--- parse.py
+++ parse.py
@@ -25,7 +25,3 @@ def read_dimacs(filename: str):
             clause = [int(literal) for literal in line.split()[:-1]]
             clauses.append(clause)
     return clauses, n_vars, n_clauses
-
-
-clauses, n_vars, n_clauses = read_dimacs(filename='DIMACS_4x4/sudoku_4x4_nr_1.cnf')
-print(clauses)
