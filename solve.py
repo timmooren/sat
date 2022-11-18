dc4c0d3b89713406f883564c9777460d5bf80806
Tim Mooren <timmooren9@gmail.com>
2022-11-18 15:35:50 +0100

    solver

--- solve.py
+++ solve.py
@@ -1,26 +1,37 @@
 from dpll import DPLL
 import pathlib
 import time
+import csv
 
 
 def main():
-    path = pathlib.Path('DIMACS_9x9')
-    # read all 4x4 files in directory
-    files = path.glob('*.cnf')
     repeats = 10
-    times = []
-    for file in files:
-        solver = DPLL()
-        solver.read_dimacs(file)
-        for _ in range(repeats):
-            start = time.perf_counter()
-            satisfaction = solver.solve()
-            end = time.perf_counter()
-            times.append(end - start)
-        assert satisfaction
-    print(f'Average time: {sum(times) / len(times)}')
+    data = [
+        ['path', 'size' 'time', 'steps']
+    ]
+    sizes = ['4x4', '9x9', '16x16']
 
+    for size in sizes:
+        path = pathlib.Path(f'DIMACS_{size}')
+        print(path)
+        files = path.glob('*.cnf')
 
+        for file in files:
+            print(file)
+            solver = DPLL()
+            solver.read_dimacs(file)
+
+            for _ in range(repeats):
+                start = time.perf_counter()
+                satisfaction = solver.solve()
+                end = time.perf_counter()
+                time = end - start
+                data.append([file, size, time, solver.step])
+                assert satisfaction
+
+    with open('data.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 
 
 if __name__ == '__main__':
